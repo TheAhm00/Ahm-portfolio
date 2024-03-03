e62e926aa4ea3e562ffae42817d315d581802b7e
Hashim1212 <angsahashim1659@gmail.com>
2024-03-03 18:30:17 +0800

    first commit

---
+++ assets/js/skill.bar.js
@@ -0,0 +1,16 @@
+(function ($) {
+    "use strict";
+
+    var offsetTop = $('#skills').offset().top;
+    $(window).on('scroll', function () {
+        var height = $(window).height();
+        if ($(window).scrollTop() + height > offsetTop) {
+            jQuery('.skillbar').each(function () {
+                jQuery(this).find('.skillbar-bar').animate({
+                    width: jQuery(this).attr('data-percent')
+                }, 1000);
+            });
+        }
+    });
+
+}(jQuery));
